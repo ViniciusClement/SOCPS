3dce50f36a919cb20d5a8b4ee813e529364c6a74
Vinicius Clemente <viniciusclemente9680@gmail.com>
2021-05-31 21:08:07 -0300

    update

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
+<!DOCTYPE html>
+
 <html>
+
 	<head>
 		<title>Minha Pagina SOCPS</title>
 	</head>
 	<body>
-		<h1><?php echo"hello world"date("d,m,y"); ?></h1>	
+		<h1>
+			<?php echo"hello world".date("d,m,y"); ?>
+		</h1>	
 
 	</body>
 
